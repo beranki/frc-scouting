042f7cd8acd9bbdc48fd7cac83b1506373c7f7c1
Shine Chang <shine00chang@gmail.com>
2024-03-13 22:26:04 -0700

    (fix) db

--- src/routes/+server.js
+++ src/routes/+server.js
@@ -1,7 +1,9 @@
 import { error } from '@sveltejs/kit';
 import { validate } from '$lib/config.js';
-import { MongoClient } from 'mongodb';
-import { MONGODB_URI } from "$env/static/private";
+import db from '$lib/db.js';
+
+const coll = db.collection("data");
+
 
 export async function POST({ request }) {
   
@@ -15,17 +17,8 @@ export async function POST({ request }) {
     error(400, 'forum invalid: ' + msg);
   }
 
-  MongoClient.connect(MONGODB_URI, async function(err, client) {
-    if (err) throw err;
-
-    console.log('Connected');
-
-    var db = client.db("scouting-data");
-    var coll = db.collection("scouting-data");
-
-    const result = await coll.insertOne(forum);
-    console.log("REASULT", result);
-  });
+  const result = await coll.insertOne(forum);
+  console.log("RESULT", result);
 
   // Defaults to 200
   return new Response('passed!');
