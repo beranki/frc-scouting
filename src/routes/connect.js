b47021d478643f417bfb0e36dd3aa0b48463cc63
Bhargav Eranki <77950550+beranki@users.noreply.github.com>
2024-03-17 10:25:43 -0700

    ss

---
+++ src/routes/connect.js
@@ -0,0 +1,22 @@
+import { MongoClient } from 'mongodb';
+ 
+// Replace the following with your Atlas connection string                                                                                                                                        
+const url = '...';
+
+// Connect to your Atlas cluster
+const client = new MongoClient(url);
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("Successfully connected to Atlas");
+
+    } catch (err) {
+        console.log(err.stack);
+    }
+    finally {
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
